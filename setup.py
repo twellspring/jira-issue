58f4cb6efef39bd10ce6dd009405f04683bf9788
Todd Wells <twells@riskalyze.com>
2018-05-31 09:01:55 -0700

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jira-issue",
-    version="0.0.3",
+    version="0.0.4",
     author="Todd Wells",
     author_email="todd@wells.ws",
     description="Quickly create jira issues from the command line",
